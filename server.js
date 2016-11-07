cd69da44871c6430017698e4ed603a38038bc4c7
priyansh gupta <priyanshgupta1161@gmail.com>
2016-11-07 21:28:52 +0530

    bug fix

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var path = require('path');
-var favicon = require('static-favicon');
+// var favicon = require('static-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
@@ -23,7 +23,7 @@ app.listen(port, function() {
     console.log('App listening on port 3000!');
 });
 
- app.use(favicon());
+ // app.use(favicon());
 
 app.use(logger('dev'));
 app.use(bodyParser.json());
