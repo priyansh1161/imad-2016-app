45d1788c6b91cb9959dfbb914046a2cbe4f66c97
priyansh gupta <priyanshgupta1161@gmail.com>
2016-11-08 01:08:30 +0530

    reset

--- server.js
+++ server.js
@@ -1,73 +1,24 @@
 var express = require('express');
+var morgan = require('morgan');
 var path = require('path');
-// var favicon = require('static-favicon');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
-var passport = require('passport');
-var session =  require('express-session');
-// var mongoose = require('mongoose');
-
-// var routes = require('./routes/index');
-// var users = require('./routes/users');
-// var auth = require('./routes/auth');
-// var polls = require('./routes/polls');
 
 var app = express();
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
-// mongoose.connect('mongodb://test:test@ds011785.mlab.com:11785/vote8er');
-var port = process.env.PORT || 3000;
-app.listen(port, function() {
-    console.log('App listening on port 3000!');
+app.use(morgan('combined'));
+
+app.get('/', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
- // app.use(favicon());
-
-app.use(logger('dev'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded());
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
-app.use(session({ secret : 'x44'
-                ,resave: true
-                 ,saveUninitialized: true }));
-
-// require('./config/passport')(app); // setting passport and adding its stategies
-app.use('/', routes);
-// app.use('/users', users);
-// app.use('/auth',auth);
-// app.use('/polls',polls);
-/// catch 404 and forwarding to error handler
-app.use(function(req, res, next) {
-    var err = new Error('Not Found');
-    err.status = 404;
-    next(err);
+app.get('/ui/style.css', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
 
-/// error handlers
-
-// development error handler
-// will print stacktrace
-if (app.get('env') === 'development') {
-    app.use(function(err, req, res, next) {
-        res.status(err.status || 500);
-        res.render('error', {
-            message: err.message,
-            error: err
-        });
-    });
-}
-
-// production error handler
-// no stacktraces leaked to user
-app.use(function(err, req, res, next) {
-    res.status(err.status || 500);
-    res.render('error', {
-        message: err.message,
-        error: {}
-    });
+app.get('/ui/madi.png', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
 });
 
 
+var port = 8080; // Use 8080 for local development because you might already have apache running on 80
+app.listen(8080, function () {
+  console.log(`IMAD course app listening on port ${port}!`);
+});
